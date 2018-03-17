e249e05effd4d781836c4afddde5ae4932bdbbf3
Oblfakir <misha-q@mail.ru>
2018-03-17 20:19:03 +0400

    init commit

---
+++ blogcore.Web/Controllers/ArticlesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blogcore.BLL.Interfaces;
+using blogcore.Entities;
+using blogcore.Web.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace blogcore.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Articles")]
+    public class ArticlesController : Controller
+    {
+        private readonly IArticleBLL _articleBll;
+
+        public ArticlesController(IArticleBLL articleBll)
+        {
+            _articleBll = articleBll;
+        }
+        
+        [HttpGet]
+        public IEnumerable<ArticleEntity> Get()
+        {
+            return _articleBll.GetAllArticles();
+        }
+        
+        [HttpGet("{id}", Name = "Get")]
+        public ArticleEntity Get(int id)
+        {
+            return _articleBll.GetArticleById(id);
+        }
+        
+        [HttpPost]
+        public void Post(JObject value)
+        {
+            var user = value.ToObject<ArticleViewModel>();
+            var a = 123;
+        }
+        
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ blogcore.Web/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using blogcore.BLL.Implementations;
+using blogcore.BLL.Interfaces;
+using blogcore.DAL.Implementations;
+using blogcore.DAL.Interfaces;
+using blogcore.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace blogcore.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            AppConfig.ConnectionString = Configuration["Production:MySqlConnectionString"];
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IArticleDAL, ArticleDAL>();
+            services.AddScoped<IArticleBLL, ArticleBLL>();
+
+            services.AddCors();
+            services.AddMvc();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
